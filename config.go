99d763a47c80701d4c5570cdf096cab7d1131c1d
mchalczynski <marcin.chalczynski@open-rnd.pl>
2016-06-15 16:52:39 +0200

    config: set default devadm url
    Issues: MEN-394
    
    Signed-off-by: mchalczynski <marcin.chalczynski@open-rnd.pl>

--- config.go
+++ config.go
@@ -37,5 +37,6 @@ var (
 		{SettingListen, SettingListenDefault},
 		{SettingMiddleware, SettingMiddlewareDefault},
 		{SettingDb, SettingDbDefault},
+		{SettingDevAdmUrlAdd, SettingDevAdmUrlAddDefault},
 	}
 )
