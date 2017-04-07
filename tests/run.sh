83e3b3f68b5260a1d4cd3488d75dfa8ce269ba7d
Maciej Borzecki <maciej.borzecki@rndity.com>
2017-04-07 09:55:05 +0200

    tests/run: log when delaying tests startup in a container
    Signed-off-by: Maciej Borzecki <maciej.borzecki@rndity.com>
    
    Changelog: None

--- tests/run.sh
+++ tests/run.sh
@@ -9,7 +9,10 @@ export PYTHONDONTWRITEBYTECODE=1
 HOST=${HOST="mender-device-auth:8080"}
 
 # if we're running in a container, wait a little before starting tests
-[ $$ -eq 1 ] && sleep 10
+[ $$ -eq 1 ] && {
+    echo "-- running in container, wait for other services"
+    sleep 10
+}
 
 py.test-3 -s --tb=short --host $HOST \
           --spec $DIR/internal_api.yml \
