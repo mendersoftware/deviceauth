7919fc98ac62d548f89a15cee9e11d76925ec466
Maciej Borzecki <maciej.borzecki@rndity.com>
2017-10-06 12:44:08 +0200

    tests: update TestLimits to use fixtures
    Changelog: None
    
    Signed-off-by: Maciej Borzecki <maciej.borzecki@rndity.com>

--- tests/tests/test_limits.py
+++ tests/tests/test_limits.py
@@ -5,42 +5,45 @@ import bravado
 import pytest
 import bravado_core
 
+from common import internal, clean_db, mongo
+
 from client import InternalClient, SimpleInternalClient
 import mockserver
 
-class TestLimits(InternalClient):
+@pytest.mark.usefixtures("clean_db")
+class TestLimits:
 
-    def test_get_limit_default_limit(self):
-        limit = self.get_max_devices_limit('foo')
+    def test_get_limit_default_limit(self, internal):
+        limit = internal.get_max_devices_limit('foo')
         assert limit.limit == 0
 
-    def test_put_limit(self):
+    def test_put_limit(self, internal):
         max_devs = 100
-        _, http_rsp = self.put_max_devices_limit('foo', max_devs).result()
+        _, http_rsp = internal.put_max_devices_limit('foo', max_devs).result()
         assert isinstance(http_rsp, bravado_core.response.IncomingResponse)
         assert http_rsp.status_code == 204
 
-    def test_limit(self):
+    def test_limit(self, internal):
         max_devs = 10
-        _, http_rsp = self.put_max_devices_limit('foo', max_devs).result()
+        _, http_rsp = internal.put_max_devices_limit('foo', max_devs).result()
         assert isinstance(http_rsp, bravado_core.response.IncomingResponse)
         assert http_rsp.status_code == 204
 
-        limit = self.get_max_devices_limit('foo')
+        limit = internal.get_max_devices_limit('foo')
         assert limit.limit == max_devs
 
-    def test_limit_differnt_tenants(self):
+    def test_limit_differnt_tenants(self, internal):
         max_devs = 10
-        _, http_rsp = self.put_max_devices_limit('foo', max_devs).result()
+        _, http_rsp = internal.put_max_devices_limit('foo', max_devs).result()
         assert isinstance(http_rsp, bravado_core.response.IncomingResponse)
         assert http_rsp.status_code == 204
 
-        limit = self.get_max_devices_limit('bar')
+        limit = internal.get_max_devices_limit('bar')
         assert limit.limit == 0
 
-    def test_put_limit_malformed_limit(self):
+    def test_put_limit_malformed_limit(self, internal):
         try:
-            _, http_rsp = self.put_max_devices_limit('foo', '1').result()
+            _, http_rsp = internal.put_max_devices_limit('foo', '1').result()
         except bravado.exception.HTTPError as herr:
             assert herr.response.status_code == 400
         else:
