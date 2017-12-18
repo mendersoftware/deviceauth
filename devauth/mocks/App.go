445b525ce9c2d3e85148e48c03a9d41b147bcd2a
Krzysztof Jaskiewicz <krzysiekjaskiewicz@gmail.com>
2017-12-18 16:24:18 +0100

    devauth: add DeleteAuthSet method
    Method for deleteing device authentication set with given id and device
    id.
    Deleting accepted authentication set results in deleting device tokens.
    If there is only one authentication set for the device, the device
    will also be deleted.
    
    changelog: none
    
    Signed-off-by: Krzysztof Jaskiewicz <krzysiekjaskiewicz@gmail.com>

--- devauth/mocks/App.go
+++ devauth/mocks/App.go
@@ -51,6 +51,20 @@ func (_m *App) DecommissionDevice(ctx context.Context, dev_id string) error {
 	return r0
 }
 
+// DeleteAuthSet provides a mock function with given fields: ctx, dev_id, auth_id
+func (_m *App) DeleteAuthSet(ctx context.Context, dev_id string, auth_id string) error {
+	ret := _m.Called(ctx, dev_id, auth_id)
+
+	var r0 error
+	if rf, ok := ret.Get(0).(func(context.Context, string, string) error); ok {
+		r0 = rf(ctx, dev_id, auth_id)
+	} else {
+		r0 = ret.Error(0)
+	}
+
+	return r0
+}
+
 // GetDevCountByStatus provides a mock function with given fields: ctx, status
 func (_m *App) GetDevCountByStatus(ctx context.Context, status string) (int, error) {
 	ret := _m.Called(ctx, status)
