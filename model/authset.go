55c254a121a377dabe3fac9f73cb91f1cfbceac5
mchalczynski <marcin.chalczynski@rndity.com>
2018-07-04 15:14:52 +0200

    model: add deviceadm-specific auth set representation
    and mapping between auth vs adm structures
    
    changelog: none
    
    Signed-off-by: mchalczynski <marcin.chalczynski@rndity.com>

--- model/authset.go
+++ model/authset.go
@@ -14,7 +14,10 @@
 package model
 
 import (
+	"encoding/json"
 	"time"
+
+	"github.com/pkg/errors"
 )
 
 const (
@@ -41,4 +44,34 @@ type AuthSetUpdate struct {
 	Timestamp         *time.Time `bson:"ts,omitempty"`
 	Status            string     `bson:"status,omitempty"`
 	AdmissionNotified *bool      `bson:"admission_notified,omitempty"`
+}
+
+type DevAdmAuthSet struct {
+	Id             string                 `json:"id" bson:"_id,omitempty"`
+	DeviceIdentity string                 `json:"device_identity" bson:"id_data"`
+	Key            string                 `json:"key" bson:"pubkey"`
+	DeviceId       string                 `json:"device_id" bson:"device_id,omitempty"`
+	RequestTime    *time.Time             `json:"request_time" bson:"request_time"`
+	Status         string                 `json:"status" bson:"status"`
+	Attributes     map[string]interface{} `json:"attributes" bson:"attributes"`
+}
+
+func NewDevAdmAuthSet(a AuthSet) (*DevAdmAuthSet, error) {
+	as := &DevAdmAuthSet{
+		Id:             a.Id,
+		DeviceIdentity: a.IdData,
+		Key:            a.PubKey,
+		DeviceId:       a.DeviceId,
+		RequestTime:    a.Timestamp,
+		Status:         a.Status,
+	}
+
+	// we don't store decoded attributes, but we will
+	// decode them on the fly for the time being
+	err := json.Unmarshal([]byte(a.IdData), &as.Attributes)
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to decode attributes for device %s, auth set", a.DeviceId, a.Id)
+	}
+
+	return as, nil
 }
