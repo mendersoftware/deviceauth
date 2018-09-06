af00755893d29532f86f82dcd24558a90aa814cb
mchalczynski <marcin.chalczynski@rndity.com>
2018-09-06 12:23:56 +0200

    tests: preauth api: check for invalid keys
    changelog: none
    
    Signed-off-by: mchalczynski <marcin.chalczynski@rndity.com>

--- tests/tests/test_preauth.py
+++ tests/tests/test_preauth.py
@@ -59,6 +59,28 @@ UwIDAQAB
         assert compare_keys(auth_set.pubkey, key)
         assert auth_set.status == 'preauthorized'
 
+    def _test_bad_key(self, management_api, devices, **kwargs):
+        aid = '1'
+        device_id = '2'
+        key = '''-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAzogVU7RGDilbsoUt/DdH
+VJvcepl0A5+xzGQ50cq1VE/Dyyy8Zp0jzRXCnnu9nu395mAFSZGotZVr+sWEpO3c
+yC3VmXdBZmXmQdZqbdD/GuixJOYfqta2ytbIUPRXFN7/I7sgzxnXWBYXYmObYvdP
+okP0mQanY+WKxp7Q16pt1RoqoAd0kmV39g13rFl35muSHbSBoAW3GBF3gO+mF5Ty
+1ddp/XcgLOsmvNNjY+2HOD5F/RX0fs07mWnbD7x+xz7KEKjF+H7ZpkqCwmwCXaf0
+iyYyh1852rti3Afw4mDxuVSD7sd9ggvYMc0QHIpQNkD4YWOhNiE1AB0zH57VbUYG
+UwIDAQAB
+'''
+        iddata = '{"foo":"bar"}'
+
+        req = management_api.make_preauth_req(aid, device_id, iddata, key)
+
+        try:
+            _, rsp = management_api.preauthorize(req, **kwargs)
+        except bravado.exception.HTTPError as e:
+            assert e.status_code == 400
+            assert e.swagger_result.error == 'failed to decode preauth request: cannot decode public key'
+
     def _test_conflict(self, management_api, devices, **kwargs):
         existing = devices[0][0]
         req = management_api.make_preauth_req('1', '2', existing.identity, existing.public_key)
@@ -81,6 +103,10 @@ class TestManagementPreauthorize(TestManagementPreauthorizeBase):
     def test_conflict(self, management_api, devices):
         self._test_conflict(management_api, devices)
 
+    @pytest.mark.parametrize('devices', ['5'], indirect=True)
+    def test_bad_key(self, management_api, devices):
+        self._test_bad_key(management_api, devices)
+
 class TestManagementPreauthorizeMultiTenant(TestManagementPreauthorizeBase):
     @pytest.mark.parametrize('tenant_foobar_devices', ['5'], indirect=True)
     def test_ok(self, management_api, tenant_foobar_devices, tenant_foobar):
@@ -91,3 +117,8 @@ class TestManagementPreauthorizeMultiTenant(TestManagementPreauthorizeBase):
     def test_conflict(self, management_api, tenant_foobar_devices, tenant_foobar):
         auth = 'Bearer ' + tenant_foobar
         self._test_conflict(management_api, tenant_foobar_devices, Authorization=auth)
+
+    @pytest.mark.parametrize('tenant_foobar_devices', ['5'], indirect=True)
+    def test_bad_key(self, management_api, tenant_foobar_devices, tenant_foobar):
+        auth = 'Bearer ' + tenant_foobar
+        self._test_bad_key(management_api, tenant_foobar_devices, Authorization=auth)
