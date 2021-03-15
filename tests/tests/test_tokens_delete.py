92b894aace3c27985f3d2eae29c311dbd1018d27
Alf-Rune Siqveland <alf.rune@northern.tech>
2021-03-15 13:20:59 +0100

    tests: Fix test_token_delete.py tests
    The tests used incorrect tenant id when mocking device JWTs.
    
    changelog: none
    
    Signed-off-by: Alf-Rune Siqveland <alf.rune@northern.tech>

--- tests/tests/test_tokens_delete.py
+++ tests/tests/test_tokens_delete.py
@@ -15,12 +15,23 @@ import bravado
 import pytest
 import requests
 import json
+import base64
 
-from common import Device, DevAuthorizer, device_auth_req, \
-    explode_jwt, \
-    clean_migrated_db, clean_db, mongo, cli, \
-    management_api, internal_api, device_api, \
-    get_fake_tenantadm_addr, make_fake_tenant_token
+from common import (
+    Device,
+    DevAuthorizer,
+    device_auth_req,
+    explode_jwt,
+    clean_migrated_db,
+    clean_db,
+    mongo,
+    cli,
+    management_api,
+    internal_api,
+    device_api,
+    get_fake_tenantadm_addr,
+    make_fake_tenant_token,
+)
 
 from cryptutil import compare_keys
 
@@ -29,32 +40,52 @@ import mockserver
 
 
 def request_token(device, dev_auth, url):
+    tenant_id = ""
+    if isinstance(dev_auth.tenant_token, str):
+        jwt_b64 = dev_auth.tenant_token.split(".")
+        if len(jwt_b64) > 1:
+            # Convert base64 from url- to std-encoding and append padding
+            claims_b64 = jwt_b64[1].replace("+", "-").replace("?", "_") + (
+                "=" * ((4 - (len(dev_auth.tenant_token) % 4)) % 4)
+            )
+            claims = base64.b64decode(claims_b64)
+            d = json.loads(claims)
+            tenant_id = d.get("mender.tenant", tenant_id)
     handlers = [
-        ('POST', '/api/internal/v1/tenantadm/tenants/verify',
-         lambda _: (200, {}, {
-             'id': '507f191e810c19729de860ea',
-             'name': 'Acme',
-         })),
+        (
+            "POST",
+            "/api/internal/v1/tenantadm/tenants/verify",
+            lambda _: (
+                200,
+                {},
+                {
+                    "id": tenant_id,
+                    "name": "Acme",
+                },
+            ),
+        ),
     ]
-    with mockserver.run_fake(get_fake_tenantadm_addr(),
-                            handlers=handlers) as fake:
+    with mockserver.run_fake(get_fake_tenantadm_addr(), handlers=handlers) as fake:
         rsp = device_auth_req(url, dev_auth, device)
         assert rsp.status_code == 200
     dev_auth.parse_rsp_payload(device, rsp.text)
     return device.token
 
+
 def verify_token(token, status_code, url):
-    auth_hdr = 'Bearer {}'.format(token)
-    rsp = requests.post(url, data='',
-            headers={'Authorization': auth_hdr})
+    auth_hdr = "Bearer {}".format(token)
+    rsp = requests.post(url, data="", headers={"Authorization": auth_hdr})
     assert rsp.status_code == status_code
 
-@pytest.yield_fixture(scope='function')
-def accepted_tenants_devices(device_api, management_api, clean_migrated_db, cli, request):
+
+@pytest.yield_fixture(scope="function")
+def accepted_tenants_devices(
+    device_api, management_api, clean_migrated_db, cli, request
+):
     """Fixture that sets up an accepted devices for tenants. The fixture can
-       be parametrized with a tenants, number of devices and number of authentication sets.
-       Yields a dict:
-       [tenant ID: [device object, ...], ]"""
+    be parametrized with a tenants, number of devices and number of authentication sets.
+    Yields a dict:
+    [tenant ID: [device object, ...], ]"""
 
     requested = request.param
 
@@ -74,25 +105,34 @@ def accepted_tenants_devices(device_api, management_api, clean_migrated_db, cli,
 
                 # poke devauth so that device appears
                 handlers = [
-                    ('POST', '/api/internal/v1/tenantadm/tenants/verify',
-                     lambda _: (200, {}, {
-                         'id': '507f191e810c19729de860ea',
-                         'name': 'Acme',
-                     })),
+                    (
+                        "POST",
+                        "/api/internal/v1/tenantadm/tenants/verify",
+                        lambda _: (
+                            200,
+                            {},
+                            {
+                                "id": "507f191e810c19729de860ea",
+                                "name": "Acme",
+                            },
+                        ),
+                    ),
                 ]
 
                 try:
                     with orchestrator.run_fake_for_device_id(1) as server:
-                        with mockserver.run_fake(get_fake_tenantadm_addr(),
-                                                handlers=handlers) as fake:
+                        with mockserver.run_fake(
+                            get_fake_tenantadm_addr(), handlers=handlers
+                        ) as fake:
                             rsp = device_auth_req(url, da, d)
                             assert rsp.status_code == 401
                 except bravado.exception.HTTPError as e:
                     assert e.response.status_code == 204
 
                 # try to find our devices in all devices listing
-                dev = management_api.find_device_by_identity(d.identity,
-                        Authorization='Bearer '+tenant_token)
+                dev = management_api.find_device_by_identity(
+                    d.identity, Authorization="Bearer " + tenant_token
+                )
 
                 devid = dev.id
                 for a in dev.auth_sets:
@@ -102,7 +142,9 @@ def accepted_tenants_devices(device_api, management_api, clean_migrated_db, cli,
 
                 try:
                     with orchestrator.run_fake_for_device_id(devid) as server:
-                        management_api.accept_device(devid, aid, Authorization='Bearer '+ tenant_token)
+                        management_api.accept_device(
+                            devid, aid, Authorization="Bearer " + tenant_token
+                        )
                         token = request_token(d, da, device_api.auth_requests_url)
                         assert len(token) > 0
                 except bravado.exception.HTTPError as e:
@@ -116,24 +158,28 @@ def accepted_tenants_devices(device_api, management_api, clean_migrated_db, cli,
 
 
 class TestEnterpriseDeleteTokens:
-    @pytest.mark.parametrize('accepted_tenants_devices', [[('foo', 2, 2), ('bar', 1, 3)]], indirect=True)
-    def test_delete_tokens_by_device_ok(self, accepted_tenants_devices, internal_api, management_api, device_api):
+    @pytest.mark.parametrize(
+        "accepted_tenants_devices", [[("foo", 2, 2), ("bar", 1, 3)]], indirect=True
+    )
+    def test_delete_tokens_by_device_ok(
+        self, accepted_tenants_devices, internal_api, management_api, device_api
+    ):
         td = accepted_tenants_devices
         try:
-            tenant_foo_token = make_fake_tenant_token('foo')
+            tenant_foo_token = make_fake_tenant_token("foo")
             da_foo = DevAuthorizer(tenant_token=tenant_foo_token)
-            d1_foo = td['foo'][0]
+            d1_foo = td["foo"][0]
             with orchestrator.run_fake_for_device_id(1) as server:
                 token1 = request_token(d1_foo, da_foo, device_api.auth_requests_url)
                 assert len(token1) > 0
-            d2_foo = td['foo'][1]
+            d2_foo = td["foo"][1]
             with orchestrator.run_fake_for_device_id(2) as server:
                 token2 = request_token(d2_foo, da_foo, device_api.auth_requests_url)
                 assert len(token2) > 0
 
-            tenant_bar_token = make_fake_tenant_token('bar')
+            tenant_bar_token = make_fake_tenant_token("bar")
             da_bar = DevAuthorizer(tenant_token=tenant_bar_token)
-            d1_bar = td['bar'][0]
+            d1_bar = td["bar"][0]
             with orchestrator.run_fake_for_device_id(1) as server:
                 token3 = request_token(d1_bar, da_bar, device_api.auth_requests_url)
                 assert len(token2) > 0
@@ -143,9 +189,10 @@ class TestEnterpriseDeleteTokens:
             verify_token(token2, 200, verify_url)
             verify_token(token3, 200, verify_url)
 
-            dev1 = management_api.find_device_by_identity(d1_foo.identity,
-                                                          Authorization='Bearer '+ tenant_foo_token)
-            payload = {'device_id': dev1.id, 'tenant_id': 'foo'}
+            dev1 = management_api.find_device_by_identity(
+                d1_foo.identity, Authorization="Bearer " + tenant_foo_token
+            )
+            payload = {"device_id": dev1.id, "tenant_id": "foo"}
             rsp = requests.delete(internal_api.make_api_url("/tokens"), params=payload)
             assert rsp.status_code == 204
 
@@ -155,26 +202,29 @@ class TestEnterpriseDeleteTokens:
         except bravado.exception.HTTPError as e:
             assert e.response.status_code == 204
 
-
-    @pytest.mark.parametrize('accepted_tenants_devices', [[('foo', 2, 2), ('bar', 1, 3)]], indirect=True)
-    def test_delete_tokens_by_non_existent_device_ok(self, accepted_tenants_devices, internal_api, management_api, device_api):
+    @pytest.mark.parametrize(
+        "accepted_tenants_devices", [[("foo", 2, 2), ("bar", 1, 3)]], indirect=True
+    )
+    def test_delete_tokens_by_non_existent_device_ok(
+        self, accepted_tenants_devices, internal_api, management_api, device_api
+    ):
         try:
             td = accepted_tenants_devices
 
-            tenant_foo_token = make_fake_tenant_token('foo')
+            tenant_foo_token = make_fake_tenant_token("foo")
             da_foo = DevAuthorizer(tenant_token=tenant_foo_token)
-            d1_foo = td['foo'][0]
+            d1_foo = td["foo"][0]
             with orchestrator.run_fake_for_device_id(1) as server:
                 token1 = request_token(d1_foo, da_foo, device_api.auth_requests_url)
                 assert len(token1) > 0
-            d2_foo = td['foo'][1]
+            d2_foo = td["foo"][1]
             with orchestrator.run_fake_for_device_id(2) as server:
                 token2 = request_token(d2_foo, da_foo, device_api.auth_requests_url)
                 assert len(token2) > 0
 
-            tenant_bar_token = make_fake_tenant_token('bar')
+            tenant_bar_token = make_fake_tenant_token("bar")
             da_bar = DevAuthorizer(tenant_token=tenant_bar_token)
-            d1_bar = td['bar'][0]
+            d1_bar = td["bar"][0]
             with orchestrator.run_fake_for_device_id(1) as server:
                 token3 = request_token(d1_bar, da_bar, device_api.auth_requests_url)
                 assert len(token2) > 0
@@ -184,7 +234,7 @@ class TestEnterpriseDeleteTokens:
             verify_token(token2, 200, verify_url)
             verify_token(token3, 200, verify_url)
 
-            payload = {'device_id': 'foo', 'tenant_id': 'foo'}
+            payload = {"device_id": "foo", "tenant_id": "foo"}
             rsp = requests.delete(internal_api.make_api_url("/tokens"), params=payload)
             assert rsp.status_code == 204
 
@@ -194,25 +244,29 @@ class TestEnterpriseDeleteTokens:
         except bravado.exception.HTTPError as e:
             assert e.response.status_code == 204
 
-    @pytest.mark.parametrize('accepted_tenants_devices', [[('foo', 2, 2), ('bar', 1, 3)]], indirect=True)
-    def test_delete_tokens_by_tenant_ok(self, accepted_tenants_devices, internal_api, management_api, device_api):
+    @pytest.mark.parametrize(
+        "accepted_tenants_devices", [[("foo", 2, 2), ("bar", 1, 3)]], indirect=True
+    )
+    def test_delete_tokens_by_tenant_ok(
+        self, accepted_tenants_devices, internal_api, management_api, device_api
+    ):
         try:
             td = accepted_tenants_devices
 
-            tenant_foo_token = make_fake_tenant_token('foo')
+            tenant_foo_token = make_fake_tenant_token("foo")
             da_foo = DevAuthorizer(tenant_token=tenant_foo_token)
-            d1_foo = td['foo'][0]
+            d1_foo = td["foo"][0]
             with orchestrator.run_fake_for_device_id(1) as server:
                 token1 = request_token(d1_foo, da_foo, device_api.auth_requests_url)
                 assert len(token1) > 0
-            d2_foo = td['foo'][1]
+            d2_foo = td["foo"][1]
             with orchestrator.run_fake_for_device_id(2) as server:
                 token2 = request_token(d2_foo, da_foo, device_api.auth_requests_url)
                 assert len(token2) > 0
 
-            tenant_bar_token = make_fake_tenant_token('bar')
+            tenant_bar_token = make_fake_tenant_token("bar")
             da_bar = DevAuthorizer(tenant_token=tenant_bar_token)
-            d1_bar = td['bar'][0]
+            d1_bar = td["bar"][0]
             with orchestrator.run_fake_for_device_id(1) as server:
                 token3 = request_token(d1_bar, da_bar, device_api.auth_requests_url)
                 assert len(token2) > 0
@@ -222,9 +276,10 @@ class TestEnterpriseDeleteTokens:
             verify_token(token2, 200, verify_url)
             verify_token(token3, 200, verify_url)
 
-            dev1 = management_api.find_device_by_identity(d1_foo.identity,
-                    Authorization='Bearer '+ tenant_foo_token)
-            payload = {'tenant_id': 'foo'}
+            dev1 = management_api.find_device_by_identity(
+                d1_foo.identity, Authorization="Bearer " + tenant_foo_token
+            )
+            payload = {"tenant_id": "foo"}
             rsp = requests.delete(internal_api.make_api_url("/tokens"), params=payload)
             assert rsp.status_code == 204
 
@@ -234,25 +289,29 @@ class TestEnterpriseDeleteTokens:
         except bravado.exception.HTTPError as e:
             assert e.response.status_code == 204
 
-    @pytest.mark.parametrize('accepted_tenants_devices', [[('foo', 2, 2), ('bar', 1, 3)]], indirect=True)
-    def test_delete_tokens_by_non_existent_tenant_ok(self, accepted_tenants_devices, internal_api, management_api, device_api):
+    @pytest.mark.parametrize(
+        "accepted_tenants_devices", [[("foo", 2, 2), ("bar", 1, 3)]], indirect=True
+    )
+    def test_delete_tokens_by_non_existent_tenant_ok(
+        self, accepted_tenants_devices, internal_api, management_api, device_api
+    ):
         try:
             td = accepted_tenants_devices
 
-            tenant_foo_token = make_fake_tenant_token('foo')
+            tenant_foo_token = make_fake_tenant_token("foo")
             da_foo = DevAuthorizer(tenant_token=tenant_foo_token)
-            d1_foo = td['foo'][0]
+            d1_foo = td["foo"][0]
             with orchestrator.run_fake_for_device_id(1) as server:
                 token1 = request_token(d1_foo, da_foo, device_api.auth_requests_url)
                 assert len(token1) > 0
-            d2_foo = td['foo'][1]
+            d2_foo = td["foo"][1]
             with orchestrator.run_fake_for_device_id(2) as server:
                 token2 = request_token(d2_foo, da_foo, device_api.auth_requests_url)
                 assert len(token2) > 0
 
-            tenant_bar_token = make_fake_tenant_token('bar')
+            tenant_bar_token = make_fake_tenant_token("bar")
             da_bar = DevAuthorizer(tenant_token=tenant_bar_token)
-            d1_bar = td['bar'][0]
+            d1_bar = td["bar"][0]
             with orchestrator.run_fake_for_device_id(1) as server:
                 token3 = request_token(d1_bar, da_bar, device_api.auth_requests_url)
                 assert len(token2) > 0
@@ -262,9 +321,10 @@ class TestEnterpriseDeleteTokens:
             verify_token(token2, 200, verify_url)
             verify_token(token3, 200, verify_url)
 
-            dev1 = management_api.find_device_by_identity(d1_foo.identity,
-                    Authorization='Bearer '+ tenant_foo_token)
-            payload = {'tenant_id': 'baz'}
+            dev1 = management_api.find_device_by_identity(
+                d1_foo.identity, Authorization="Bearer " + tenant_foo_token
+            )
+            payload = {"tenant_id": "baz"}
             rsp = requests.delete(internal_api.make_api_url("/tokens"), params=payload)
             assert rsp.status_code == 204
 
@@ -279,6 +339,6 @@ class TestEnterpriseDeleteTokens:
         assert rsp.status_code == 400
 
     def test_delete_tokens_by_device_no_tenant_id_bad_request(self, internal_api):
-        payload = {'device_id': 'foo'}
+        payload = {"device_id": "foo"}
         rsp = requests.delete(internal_api.make_api_url("/tokens"), params=payload)
         assert rsp.status_code == 400
