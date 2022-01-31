608e3ebce916ff30c0ad8de1669fb38b54fa5c32
Alf-Rune Siqveland <alf.rune@northern.tech>
2022-01-31 16:59:46 +0100

    chore(vendor): Migrate to community maintained jwt package
    changelog: none
    
    Signed-off-by: Alf-Rune Siqveland <alf.rune@northern.tech>

--- jwt/jwt.go
+++ jwt/jwt.go
@@ -1,4 +1,4 @@
-// Copyright 2021 Northern.tech AS
+// Copyright 2022 Northern.tech AS
 //
 //    Licensed under the Apache License, Version 2.0 (the "License");
 //    you may not use this file except in compliance with the License.
@@ -16,7 +16,7 @@ package jwt
 import (
 	"crypto/rsa"
 
-	jwtgo "github.com/dgrijalva/jwt-go"
+	jwtgo "github.com/golang-jwt/jwt/v4"
 	"github.com/pkg/errors"
 )
 
