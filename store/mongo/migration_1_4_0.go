5cd06a068cfcfa87952fdc58c3328bea33b2976c
Peter Grzybowski <peter@northern.tech>
2023-04-04 08:25:11 +0200

    chore: moving to single db
    Ticket: MEN-4745
    Signed-off-by: Peter Grzybowski <peter@northern.tech>

--- store/mongo/migration_1_4_0.go
+++ store/mongo/migration_1_4_0.go
@@ -1,16 +1,16 @@
-// Copyright 2019 Northern.tech AS
+// Copyright 2023 Northern.tech AS
 //
-//    Licensed under the Apache License, Version 2.0 (the "License");
-//    you may not use this file except in compliance with the License.
-//    You may obtain a copy of the License at
+//	Licensed under the Apache License, Version 2.0 (the "License");
+//	you may not use this file except in compliance with the License.
+//	You may obtain a copy of the License at
 //
-//        http://www.apache.org/licenses/LICENSE-2.0
+//	    http://www.apache.org/licenses/LICENSE-2.0
 //
-//    Unless required by applicable law or agreed to in writing, software
-//    distributed under the License is distributed on an "AS IS" BASIS,
-//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//    See the License for the specific language governing permissions and
-//    limitations under the License.
+//	Unless required by applicable law or agreed to in writing, software
+//	distributed under the License is distributed on an "AS IS" BASIS,
+//	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//	See the License for the specific language governing permissions and
+//	limitations under the License.
 package mongo
 
 import (
@@ -47,7 +47,13 @@ func (m *migration_1_4_0) Up(from migrate.Version) error {
 			continue
 		}
 
-		status, err := m.ms.GetDeviceStatus(m.ctx, dev.Id)
+		status, err := getDeviceStatusDB(
+			m.ms,
+			ctxstore.DbFromContext(m.ctx, DbName),
+			"",
+			m.ctx,
+			dev.Id,
+		)
 
 		if err != nil {
 			if err == store.ErrAuthSetNotFound {
