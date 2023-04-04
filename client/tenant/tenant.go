a8f84cebb278efc46a6b12466de70aeba69da0de
Peter Grzybowski <peter@northern.tech>
2023-04-04 08:24:50 +0200

    chore: moving to single db: fmt
    Ticket: MEN-4745
    Signed-off-by: Peter Grzybowski <peter@northern.tech>

--- client/tenant/tenant.go
+++ client/tenant/tenant.go
@@ -1,4 +1,4 @@
-// Copyright 2021 Northern.tech AS
+// Copyright 2023 Northern.tech AS
 //
 //    Licensed under the Apache License, Version 2.0 (the "License");
 //    you may not use this file except in compliance with the License.
@@ -19,7 +19,7 @@ import (
 	"github.com/mendersoftware/go-lib-micro/ratelimits"
 )
 
-//Tenant is a tenantadm-specific API struct
+// Tenant is a tenantadm-specific API struct
 type Tenant struct {
 	ID        string          `json:"id"`
 	Name      string          `json:"name"`
--- utils/crypto.go
+++ utils/crypto.go
@@ -1,4 +1,4 @@
-// Copyright 2020 Northern.tech AS
+// Copyright 2023 Northern.tech AS
 //
 //    Licensed under the Apache License, Version 2.0 (the "License");
 //    you may not use this file except in compliance with the License.
@@ -93,7 +93,7 @@ func VerifyAuthReqSign(signature string, pubkey crypto.PublicKey, content []byte
 	return nil
 }
 
-//ParsePubKey
+// ParsePubKey
 func ParsePubKey(pubkey string) (crypto.PublicKey, error) {
 	block, _ := pem.Decode([]byte(pubkey))
 	if block == nil || block.Type != PubKeyBlockType {
